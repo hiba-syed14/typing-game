4f27bd2ecf528f718a0c0c44f3df30885aa6abbd
Hiba Syed <hiba.syed@hotmail.com>
2023-06-27 20:14:19 -0400

    more js edits

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const quotes = [
+  'Things are only impossible until they are not',
+  'It is possible to commit no errors and still lose. That is not a weakness. That is life',
+  'There is a way out of every box, a solution to every puzzle; it is just a matter of finding it.',
+  'Without freedom of choice there is no creativity',
+  'Logic is the beginning of wisdom, not the end',
+  'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+  'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+];
 const quote = document.getElementById('quote'); // <p id="quote"
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start'); // <button id="start"
+const message= document.getElementById('message');
+
 
 let quoteText; // undefined
 let wordQueue;
 let highlightPosition;
+let startTime;
 
 function startGame() {
-  quoteText = 'type me';
-  wordQueue = quoteText.split(' '); // ['type', 'me']
-  quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join(''); // <span>type</span><span>me</span>
+  const quoteIndex= Math.floor(Math.random()*quotes.length);
+  const quoteText=quotes[quoteIndex];
+  wordQueue = quoteText.split(' '); 
+  quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join(''); 
   
   highlightPosition = 0;
   // quote.childNodes = ['<span>type</span>', '<span>me</span>']
@@ -28,6 +41,22 @@ function checkInput() {
 
   wordQueue.shift(); // remove the first word from the wordQueue array, because we already typed it!
   input.value = ''; // clear the input field
+  if (wordQueue.length=== 0) {// if we have run out of words then game over.}
+    gameOver();
+    return;
+}
+  quote.childNodes[highlightPosition].className = "";
+  highlightPosition++;
+  quote.childNodes[highlightPosition].className = "highlight";
+  
+}
+
+function gameOver() {
+  const elapsedTime= new Date().getTime() - startTime;
+  message.innerHTML=`
+  <span class"congrats">Congratulations!</span><br>
+  You finished in ${elapsedTime} seconds.
+  `;
 }
 
 start.addEventListener('click', startGame);
